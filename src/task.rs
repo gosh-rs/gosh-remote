0a50708e16ad862b8acecf220f116cc7366726fd
Wenping Guo <ybyygu@gmail.com>
2022-03-10 11:06:39 +0800

    task: generic input and output types

--- src/task.rs
+++ src/task.rs
@@ -30,35 +30,39 @@
 use super::*;
 
 use tokio::sync::{mpsc, oneshot};
+
+use std::fmt::Debug;
+use std::marker::Send;
 // 475dbc7d ends here
 
 // [[file:../remote.note::214790a9][214790a9]]
-type Computed = String;
-type TxComputed = oneshot::Sender<Computed>;
+type Computed<O> = O;
+type TxComputed<O> = oneshot::Sender<Computed<O>>;
 /// RemoteIO contains input and output for remote execution. The first field in tuple
 /// is job input, and the second is for writing job output.
-pub type RemoteIO = (String, TxComputed);
-type TxInput = mpsc::Sender<RemoteIO>;
+pub type RemoteIO<I, O> = (I, TxComputed<O>);
+type TxInput<I, O> = mpsc::Sender<RemoteIO<I, O>>;
 /// The receiver of jobs for remote execution
-pub type RxInput = mpsc::Receiver<RemoteIO>;
+pub type RxInput<I, O> = mpsc::Receiver<RemoteIO<I, O>>;
 // 214790a9 ends here
 
 // [[file:../remote.note::b55affa9][b55affa9]]
 /// The client side for remote execution
 #[derive(Debug, Clone, Default)]
-pub struct TaskSender {
-    tx_inp: Option<TxInput>,
+pub struct TaskSender<I, O> {
+    tx_inp: Option<TxInput<I, O>>,
 }
 
-impl TaskSender {
+impl<I: Debug, O: Debug + Send> TaskSender<I, O> {
     /// Ask remote side compute with `input` and return the computed.
-    pub async fn remote_compute(&self, input: impl Into<String>) -> Result<Computed> {
+    pub async fn remote_compute(&self, input: impl Into<I>) -> Result<Computed<O>> {
         let (tx, rx) = tokio::sync::oneshot::channel();
         self.tx_inp
             .as_ref()
             .expect("task input")
             .send((input.into(), tx))
-            .await?;
+            .await
+            .map_err(|err| format_err!("task send error: {err:?}"))?;
         let computed = rx.await?;
         Ok(computed)
     }
@@ -68,18 +72,18 @@ impl TaskSender {
 // [[file:../remote.note::f45eafe9][f45eafe9]]
 /// The server side for remote execution
 #[derive(Debug)]
-pub struct TaskReceiver {
-    rx_inp: RxInput,
+pub struct TaskReceiver<I, O> {
+    rx_inp: RxInput<I, O>,
 }
 
-impl TaskReceiver {
+impl<I, O> TaskReceiver<I, O> {
     /// Receives the next task for this receiver.
-    pub async fn recv(&mut self) -> Option<RemoteIO> {
+    pub async fn recv(&mut self) -> Option<RemoteIO<I, O>> {
         self.rx_inp.recv().await
     }
 }
 
-fn new_interactive_task() -> (TaskReceiver, TaskSender) {
+fn new_interactive_task<I, O>() -> (TaskReceiver<I, O>, TaskSender<I, O>) {
     let (tx_inp, rx_inp) = tokio::sync::mpsc::channel(1);
 
     let server = TaskReceiver { rx_inp };
@@ -91,12 +95,12 @@ fn new_interactive_task() -> (TaskReceiver, TaskSender) {
 
 // [[file:../remote.note::3f19ae12][3f19ae12]]
 /// A Task channel for remote execution (multi-producer, single-consumer)
-pub struct Task {
-    sender: TaskSender,
-    receiver: TaskReceiver,
+pub struct Task<I, O> {
+    sender: TaskSender<I, O>,
+    receiver: TaskReceiver<I, O>,
 }
 
-impl Task {
+impl<I, O> Task<I, O> {
     /// Create a task channel for computation of molecule in client/server
     /// architecture
     pub fn new() -> Self {
@@ -105,7 +109,7 @@ impl Task {
     }
 
     /// Splits a single task into separate read and write half
-    pub fn split(self) -> (TaskReceiver, TaskSender) {
+    pub fn split(self) -> (TaskReceiver<I, O>, TaskSender<I, O>) {
         match self {
             Self {
                 sender: tx,
